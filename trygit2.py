09f162602471210582ca9ab1ec5530cb389826b6
Dian <alhusari2@gmail.com>
2023-02-22 14:58:02 +0700

    upload file trygit2.py ke github

---
+++ trygit2.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import datasets
+
+iris = datasets.load_iris()
+df=pd.DataFrame(iris['data'])
+print(df.head())
+
+iris['target']
+
+inertias = []
+K = range(1,10)
+for k in K:
+ kmeanModel = KMeans(n_clusters=k)
+ kmeanModel.fit(df)
+ inertias.append(kmeanModel.inertia_)
+plt.figure(figsize=(16,8))
+plt.plot(K, inertias, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Inertias')
+plt.title('The Elbow Method showing the optimal k')
+plt.show()
